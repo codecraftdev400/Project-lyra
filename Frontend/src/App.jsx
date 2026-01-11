0ccf9fd51231b58b0931a093619e7449feb32edf
codecraftdev400 <af19hfa0393@student.ric.edu.pk>
2026-01-11 17:58:46 +0500

    updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
             This is a simple card layout built with Tailwind CSS.
         </p>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
-            Learn More
+            Learn 
         </button>
     </div>
   );
